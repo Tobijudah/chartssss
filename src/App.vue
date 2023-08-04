950503bd2baf4c2cce905a6f32bd5a971d209cc8
Oluwatobiju Judah <tobijudah@gmail.com>
2023-08-05 00:54:07 +0100

    feat: creates App

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <header class="header">
+      <h1 class="title">Chartssss<span class="purple">.</span></h1>
+      <h2 class="subTitle purple">Discover music charts from all over the world</h2>
+    </header>
+  </main>
+</template>
+
+<style scoped>
+.header {
+  margin: 5rem 0;
+  text-align: center;
+}
+
+.title {
+  font-size: 5rem;
+  font-family: 'Arapey', serif;
+  letter-spacing: 0;
+  margin-top: 3rem;
+}
+</style>
