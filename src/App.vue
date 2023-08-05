1ee7a7926eda65faff8cf662ec0e086a6b10e0da
Oluwatobiju Judah <tobijudah@gmail.com>
2023-08-05 13:45:37 +0100

    chore: moves header to SFC

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Header from './components/Header.vue';
+</script>
 
 <template>
   <main>
-    <header class="header">
-      <h1 class="title">Chartssss<span class="purple">.</span></h1>
-      <h2 class="subTitle purple">Discover music charts from all over the world</h2>
-    </header>
+    <Header />
   </main>
 </template>
 
-<style scoped>
-.header {
-  margin: 5rem 0;
-  text-align: center;
-}
-
-.title {
-  font-size: 5rem;
-  font-family: 'Arapey', serif;
-  letter-spacing: 0;
-  margin-top: 3rem;
-}
-</style>
+<style scoped></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header class="header">
+    <h1 class="title">Chartssss<span class="purple">.</span></h1>
+    <h2 class="purple">Discover music charts from all over the world</h2>
+  </header>
+</template>
+
+<style scoped>
+.header {
+  margin: 5rem 0;
+  text-align: center;
+}
+
+.title {
+  font-size: 5rem;
+  font-family: 'Arapey', serif;
+  letter-spacing: 0;
+  margin-top: 3rem;
+}
+</style>
