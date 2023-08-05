13a5eeaa1a35322227fdd2b1b46cef208bae85b0
Oluwatobiju Judah <tobijudah@gmail.com>
2023-08-05 13:45:55 +0100

    chore: adds main styles

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,15 @@ import Header from './components/Header.vue';
 </script>
 
 <template>
-  <main>
+  <main class="main">
     <Header />
   </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+.main {
+  width: 90%;
+  max-width: 1440px;
+  margin: 0 auto;
+}
+</style>
