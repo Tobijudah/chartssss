1e65c51a687691268a88a40b018482f7bfcccaa1
Oluwatobiju Judah <tobijudah@gmail.com>
2023-08-06 20:07:51 +0100

    chore: adds meta twitter card

---
+++ src/assets/countries.ts
@@ -0,0 +1,12 @@
+const countries = [
+  { label: 'Nigeria', value: 'ng' },
+  { label: 'United States', value: 'us' },
+  { label: 'United Kingdom', value: 'gb' },
+  { label: 'South Africa', value: 'za' },
+  { label: 'Ghana', value: 'gh' },
+  // All countries
+  { label: 'Afghanistan', value: 'af' },
+  { label: 'Albania', value: 'al' },
+  
+  
+];
